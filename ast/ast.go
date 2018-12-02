e71853eea91e7dcd1b2164154f79329deb2be8ae
Stuart Jansen <sjansen@buscaluz.org>
2018-12-01 21:12:29 -0700

    add ast.Argument interface

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
+//go:generate go run ../scripts/tmpl-to-go/main.go arguments.go.tmpl
 //go:generate go run ../scripts/tmpl-to-go/main.go parts.go.tmpl
 
 type Message struct {
 	Parts []Part
+}
+
+type Argument interface {
+	ArgNameOrNumber() string
 }
 
 type Part interface {
